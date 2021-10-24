5652a1171598a5549d8c1e8664f2547dfa3d55c8
Alexander Lvov <Al.lvov777@gmail.com>
2021-10-25 02:23:19 +0300

    Update LengthWrapperTest.cpp

--- LengthWrapperTest.cpp
+++ LengthWrapperTest.cpp
@@ -3,17 +3,6 @@
 #include "LengthWrapper.h"
 
 int main() {
-//    LengthWrapper test(16000000);
-//    std::string t;
-//    for(auto i = 1; i < test.Length(); ++i)
-//        t += std::bitset<8>(*(test.data() + i)).to_string();
-//
-//    if(t != std::bitset<24>(16000000).to_string())
-//        std::cout << "Incorrect" << std::endl;
-//
-//    std::cout << t << "    - Mine" << std::endl;
-//    std::cout << std::bitset<24>(16000000).to_string();
-
     srand(100000);
     for(auto i = 0; i < 200; ++i) {
         auto num = (((unsigned) rand() + 256) * 256) % 16777216;
